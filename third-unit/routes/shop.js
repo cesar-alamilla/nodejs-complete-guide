8201299369100a8c4af8b731dec15e7658d1ae12
cesar-alamilla <czar.5@hotmail.com>
2021-03-23 23:23:36 -0600

    archivos de tercera unidad

---
+++ third-unit/routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'Shop', 
+    path: '/', 
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
+
+module.exports = router;
